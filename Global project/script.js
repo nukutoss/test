171279b808ea68452a1a836a92efb45911f6408a
Nikita <sidorenko-nikita-1999@mail.ru>
2019-07-23 16:21:06 +0300

    This is first commit

---
+++ Global project/script.js
@@ -0,0 +1,113 @@
+'use strict'; // Современный стандарт ES5
+
+// №3
+// var leftBordenrWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(second);
+
+// ------
+
+// №4
+// let nothing = 5;
+// console.log(nothing);
+// Null и Undefint
+
+
+// let persone = {
+//     name: 'Marge',
+//     age: 45,
+//     sons: 1,
+//     pet: 54
+// }
+// Можно выводить переменные через точку или [''];
+// console.log(persone.['name']);
+// console.log(persone.age);
+// console.log(persone.sons);
+// console.log(persone.pet);
+
+// Массивы
+// let art = ['apple.png', 'orange.bmp', 'banana.jpeg', 'Mango.bbg'];
+// document.write(art[0]);
+
+// ------
+
+//Общение с пользователем №5
+// alert('Come om!');
+// let test = confirm('Are u okey?');
+
+
+
+// let name = prompt("What u name?", 'NATASHA');
+// console.log(name);
+
+// -----
+
+// №6 Операторы
+
+// console.log(4 + " - four");
+
+// let name = prompt("Do u have 18 y.o.?", '');
+// console.log(name);
+
+
+// Increment&Decrement
+
+// let incr = 10,
+//     decr = 10;
+// //Увеличивает на +1 и уменьшает на -1.
+// incr++;
+// decr--;
+// console.log('Инкримент - '+ incr);
+// console.log('Диктримент - '+ decr);
+
+// let incr = 10,
+//     decr = 10;
+// //Префиксная форма - возвращает уже измененное значение
+// console.log(++incr);
+// console.log(--decr);
+
+// let incr = 10,
+//     decr = 10;
+// //Постфиксная форма - сначала вовзращает, а потом измениет
+// console.log(incr++);
+// console.log(decr--);
+
+
+// Остаток от деления одного числа на другое
+// console.log(5%2);
+
+// = - присваивание 
+// == - проверка на равенство
+// === - строгая проверка по типам данных
+// console.log('5' == 5) //true
+// console.log('5' === 5) //false
+// console.log(5 === 5) //true
+
+// var x = 8;
+// var y = x++ * 5;
+// var z = y  % x;
+// alert(z);
+
+
+// // И и Или
+// // && - и
+// let isChecked = true,
+//     isClose = false;
+// // Оператор && - и, возвращает только если оба правдивые
+// console.log(isChecked && isClose);
+
+// || -или
+
+// let isChecked = true,
+//     isClose = true;
+// // Оператор || - или, возвращает если любой из них верен
+// console.log(isChecked || isClose);
+
+
+// ! - оператор отрицания, превращает правду в ложь и наоборот
+// let isChecked = false,
+//     isClose = true;
+
+// console.log(isChecked || !isClose);
+
